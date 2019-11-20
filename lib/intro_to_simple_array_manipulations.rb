f9cd8958bdaa54850b6f144dda7e295c72fa0f6b
Taahira Carter <taahirac001@gmail.com>
2019-11-20 01:56:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,7 +27,7 @@ def using_concat(array1, array2)
 end
 
 def using_insert(array, element)
-  array.insert(element)
+  array[3].insert(element)
 end
 
 def using_uniq(array)
@@ -43,5 +43,5 @@ def using_delete(array, string)
 end
 
 def using_delete_at(array, int)
-  array.delete(int)
+  array[int].delete
 end
